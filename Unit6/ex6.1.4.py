29e270a094bf624b3d9351960752d3ecfdf7bbc3
TheOk30 <omerkislev@gmail.com>
2024-06-03 23:43:53 +0300

    Finished Unit 6 - modules

---
+++ Unit6/ex6.1.4.py
@@ -0,0 +1,12 @@
+import base64
+
+def main():
+    msg = "CgkJICAgICAgICAgICAgICAgIAogICAgICAgICAgICAgICAuLS0tW1tfX11dLS0tLS4KICAgICAgICAgICAgICA7LS0tLS0tLS0tLS0tLS58ICAgICAgIF9fX18KICAgICAgICAgICAgICB8ICAgICAgICAgICAgIHx8ICAgLi0tW1tfX11dLS0tLgogICAgICAgICAgICAgIHwgICAgICAgICAgICAgfHwgIDstLS0tLS0tLS0tLS58CiAgICAgICAgICAgICAgfCAgICAgICAgICAgICB8fCAgfCAgICAgICAgICAgfHwKICAgICAgICAgICAgICB8X19fX19fX19fX19fX3wvICB8ICAgICAgICAgICB8fAogICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgIHxfX19fX19fX19fX3wvCgo="
+    msg_bytes = base64.b64decode(msg)
+    decoded_message = msg_bytes.decode('utf-8')
+    print(decoded_message)
+
+if __name__ == '__main__':
+    main()
+    
+# Suitcase
---
+++ Unit6/ex6.2.5/file1.py
@@ -0,0 +1,10 @@
+class GreetingCard:
+    def __init__(self, recipient = "Dana Ev", sender = "Eyal Ch"):
+        self._recipient = recipient
+        self._sender = sender
+
+    def greeting_msg(self):
+        return f"Dear {self._recipient},\n\nBest wishes,\n{self._sender}"
+
+    def __str__(self):
+        return self.greeting_msg()
